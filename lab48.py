bc5bedf5a54158e97ded071ec126e544124e8358
segarciat <segarciat314@gmail.com>
2023-01-06 16:07:42 +0000

    Text formatting changes

--- lab48.py
+++ lab48.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 MAX_GUESTS = 5
-drinks = {"COFFEE": 2.00, "TEA": 1.50, "WATER": 1.00, "JUICE": 3.00, "SODA": 2.00}
+DRINKS = {"COFFEE": 2.00, "TEA": 1.50, "WATER": 1.00, "JUICE": 3.00, "SODA": 2.00}
 
 def welcome():
     print("Welcome to Only-Drinks Cafe!")
@@ -17,36 +17,35 @@ def prompt_guest_count():
         else:
             return int(n)
 
-def prompt_for_drink(drinks):
+def prompt_for_drink():
     """Prompts guest for drink of choice"""
     while True:
-        display_options(drinks)
+        display_options()
         option = input("\nWhat would you like? ").upper()
-        for drink in drinks:
+        for drink in DRINKS:
             if option == drink or drink.startswith(option):
                 return drink
 
-def display_options(drinks):
+def display_options():
     print("These are our drink options: ")
     print("-" * 40)
-    for drink, price in drinks.items():
+    for drink, price in DRINKS.items():
         print(f"\t({drink[0]}){drink[1:]:10} - ${price:4.2f}")
 
 def take_orders(number_of_guests):
     orders = []
     for i in range(number_of_guests):
-        drink = prompt_for_drink(drinks)
+        drink = prompt_for_drink()
         orders.append(drink)
         print(f"Guest {i + 1} wants a {drink}\n")
-
-    print("\nRepeating orders: ")
-    print("-" * 30)
-    for n, drink in enumerate(orders):
-        print(f"\t{drink} for guest {n + 1}")
     return orders
 
 def display_total(orders): 
-    total = sum(drinks[order] for order in orders)
+    print("-" * 30)
+    for n, drink in enumerate(orders):
+        print(f"\t{drink:10} (${DRINKS[drink]:5.2f}) for guest {n + 1}")
+    print("-" * 30)
+    total = sum(DRINKS[order] for order in orders)
     print(f"Your total is: ${total:6.2f}")
 
 if __name__ == '__main__':
