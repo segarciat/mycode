c4d6ca3293c4d841aee1cfcc02d1f2a98883c30d
segarciat <segarciat314@gmail.com>
2023-01-06 20:30:35 +0000

    Playing with python pip packages

---
+++ if_name/first_module.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+print("This code will always execute.")
+
+def main():
+    print("This code belongs to the main function in module 1")
+
+if __name__ == '__main__':
+    main()
---
+++ if_name/second_module.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+import first_module
+
+first_module.main()
+
+print(f"Module #2 Name={__name__}")
