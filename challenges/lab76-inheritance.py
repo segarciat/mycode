9c03c99a0abb85116ad0513b4e683de07adb1f4e
segarciat <segarciat314@gmail.com>
2023-01-09 22:23:15 +0000

    Modified inheritance challenge

--- challenges/lab76-inheritance.py
+++ challenges/lab76-inheritance.py
@@ -1,12 +1,48 @@
 #!/usr/bin/env python3
 """Author: Sergio Garcia"""
 
-class D6Die:
+import random
+
+class Player:
+    """A player that can roll a die"""
+    def __init__(self):
+        self._rolls = []
+
     def roll(self):
-        return random.randint(1, 6)
+        """Rolls a D6."""
+        result = random.randint(1, 6)
+        self._rolls.append(result)
+        return result
+
+    def get_rolls(self):
+        """Returns a copy of the rolls thus far"""
+        return list(self._rolls)
 
 
-class FlipCheatD6:
+class FlipCheatD6(Player):
+    """A player who cheats by cheats by flipping a die"""
     def roll(self):
-        result = super.roll()
+        """Rolls a D6, and flips the result if unfavorable."""
+        result = super().roll()
+        # Replace last roll with value after flip.
+        self._rolls[-1] = self._flip_die(result)
+        return self._rolls[-1]
+
+    def _flip_die(self, result):
+        """When rolling 3 or lower, fip the die"""
         return result if result > 3 else 7 - result
+
+def main():
+    """Display results of rolling a fair die vs a flipped die"""
+    player1 = Player()
+    player2 = FlipCheatD6()
+
+    for i in range(10):
+        player1.roll()
+        player2.roll()
+
+    print(player1.get_rolls())
+    print(player2.get_rolls())
+
+if __name__ == '__main__':
+    main()
