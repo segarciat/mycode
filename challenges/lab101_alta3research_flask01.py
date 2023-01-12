ed68e318adef1445a0b6badb535f01e4f42d6114
segarciat <segarciat314@gmail.com>
2023-01-12 20:48:27 +0000

    Lab 101 using cookies in flask

--- challenges/lab101_alta3research_flask01.py
+++ challenges/lab101_alta3research_flask01.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python3
 
-from flask import Flask, render_template, request
+from flask import (
+    Flask, 
+    render_template, 
+    request,
+    make_response,
+    redirect,
+    url_for
+)
+
+EXPECTED = 4
 
 app = Flask(__name__)
 
@@ -14,9 +23,24 @@ def math():
         "name": request.form.get("name"),
         "addition": int(request.form.get("addition"))
     }
-    print(answers)
 
-    return render_template("math.html", **answers)
+    response = make_response(render_template("math.html", **answers))
+    response.set_cookie("name", answers.get("name"))
+    response.set_cookie("wizard", str(answers.get("addition") == EXPECTED))
+
+    return response
+
+@app.route("/hogwarts")
+def hogwarts():
+    name = request.cookies.get("name", None)
+    is_wizard = request.cookies.get("wizard")
+
+    if is_wizard == "True":
+        return f"<h1>You're a wizard, {name}</h1>"
+    else:
+        message = f"<h1>{name}? Not on the list.</h1>"
+        return message
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=2224)
